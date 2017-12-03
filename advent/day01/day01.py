2dff02c8f3dc2c280030cf7b3faf30f841f39b88
Ben White <ben.white@netlight.com>
2017-12-03 11:03:07 +0100

    Added part two solution for day01 - did not realise there are two parts per day!

--- advent/day01/day01.py
+++ advent/day01/day01.py
@@ -13,3 +13,20 @@ def reverse_captcha(txt):
         total += arr[0]
 
     return total
+
+
+def reverse_captcha_two(txt):
+    arr = [int(x) for x in list(txt)]
+    arr_length = len(arr)
+    half_way = int(arr_length * 0.5)
+    total = 0
+
+    for idx, c in enumerate(arr):
+        pointer = idx + half_way
+        if pointer >= arr_length:
+            pointer -= arr_length
+        
+        if arr[pointer] == c:
+            total += c
+
+    return total
