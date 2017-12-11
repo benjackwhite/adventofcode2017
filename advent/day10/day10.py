ee9236e8c59a3eddea19cf63f031755407493e2f
Ben White <ben.white@netlight.com>
2017-12-11 08:18:27 +0100

    Added failing step 2

--- advent/day10/day10.py
+++ advent/day10/day10.py
@@ -1,21 +1,24 @@
+from functools import reduce
+
+
 def get_circular_index(arr, index):
     if index >= len(arr):
-        return index - len(arr)
+        while index >= len(arr):
+            index -= len(arr)
 
     return index
 
 
 def circular_move(arr, pos, amount):
-    new_index = pos + amount
-    if new_index >= len(arr):
-        return new_index - len(arr)
+    index = pos + amount
+    while index >= len(arr):
+        index -= len(arr)
 
-    return new_index
+    return index
 
 
-def fn(arr, twists):
-    pos = 0
-    skip_size = 0
+def fn(arr, twists, pos = 0, skip_size = 0):
+    arr = arr[:]
     current = []
 
     for t in twists:
@@ -30,6 +33,42 @@ def fn(arr, twists):
 
         pos = circular_move(arr, pos, t + skip_size)
         skip_size += 1
+        if skip_size >= len(arr):
+            skip_size -= len(arr)
+
+    return arr, pos, skip_size
 
 
-    return arr
+def knot_hash(arr, txt):
+    pos = 0
+    skip_size = 0
+    current = []
+    standard_twists = [17, 31, 73, 47, 23]
+
+    twists = [ord(c) for c in list(txt)] + standard_twists
+
+    # print(twists)
+
+    for i in range(64):
+        # print(pos, skip_size)
+        mashed, new_pos, new_skip_size = fn(arr, twists, pos, skip_size)
+
+        # print(new_pos, new_skip_size)
+        pos = new_pos
+        skip_size = new_skip_size
+
+    dense = []
+
+    for i in range(16):
+        block = mashed[i:i+16]
+        current = 0
+
+        product = reduce((lambda x, y: x ^ y), block)
+        dense.append(product)
+
+
+    print(dense)
+    dense = ['{0:02x}'.format(x) for x in dense]
+    print(dense)
+
+    return "".join(dense)
--- advent/day10/day10_test.py
+++ advent/day10/day10_test.py
@@ -1,17 +1,28 @@
-from day10 import fn
-from data import data
+from day10 import fn, knot_hash
 
 arr256 = [x for x in range(256)]
 
 
 def test_fn():
-    changed = fn([0, 1, 2, 3, 4], [3, 4, 1, 5])
+    changed = fn([0, 1, 2, 3, 4], [3, 4, 1, 5])[0]
 
     assert changed[0] * changed[1] == 12
 
     changed = fn(
         arr256,
         [102, 255, 99, 252, 200, 24, 219, 57, 103, 2, 226, 254, 1, 0, 69, 216]
-    )
+    )[0]
 
     assert changed[0] * changed[1] == 5577
+
+
+def test_knot_hash():
+    assert knot_hash(arr256, "1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d"
+    assert knot_hash(arr256, "") == "a2582a3a0e66e6e86e3812dcb672a272"
+    assert knot_hash(arr256, "AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd"
+    assert knot_hash(arr256, "1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e"
+    
+    assert knot_hash(
+        arr256,
+        "102, 255, 99, 252, 200, 24, 219, 57, 103, 2, 226, 254, 1, 0, 69, 216"
+    ) == "wat"
